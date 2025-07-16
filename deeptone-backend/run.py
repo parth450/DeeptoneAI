71e3e7e5162b6101cde3e4de43aac3d7ab0f028d
parth kachariya <parthkachariya2004@gmail.com>
2025-07-16 19:34:37 +0530

    Update run.py

--- deeptone-backend/run.py
+++ deeptone-backend/run.py
@@ -2,14 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 from app.routes import main
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    app.register_blueprint(main)
-    return app
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     import os
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
