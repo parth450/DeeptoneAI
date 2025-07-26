eea51ea97af9eb5eddb12db8d51b036145a48a43
parth kachariya <parthkachariya2004@gmail.com>
2025-07-26 17:08:54 +0530

    Update database.py

--- deeptone-backend/app/database.py
+++ deeptone-backend/app/database.py
@@ -11,9 +11,9 @@ try:
     predictions_collection = db.get_collection("predictions")
     users_collection = db.get_collection("users")
 
-    print("✅ MongoDB connected successfully")
+    print(" MongoDB connected successfully")
 
 except Exception as e:
-    print("❌ MongoDB connection failed:", e)
+    print(" MongoDB connection failed:", e)
     predictions_collection = None
     users_collection = None
